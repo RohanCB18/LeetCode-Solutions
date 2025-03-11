4f6728543ec690c36496fc37430aff2ea21b1350
RohanCB18 <rohan.blr21@gmail.com>
2025-03-11 16:20:54 +0530

    Update LC1780_CheckPowersOfThree.java

--- LeetCode-Solutions/LC1780_CheckPowersOfThree.java
+++ LeetCode-Solutions/LC1780_CheckPowersOfThree.java
@@ -1,16 +1,16 @@
 /* 
-🎯 LeetCode 1780 - Check if Number is a Sum of Three Powers of Three
+ LeetCode 1780 - Check if Number is a Sum of Three Powers of Three
 🔹 Difficulty: Medium
 
-📝 Problem Statement:
+ Problem Statement:
 Given an integer `n`, return **true** if it can be represented as the sum of three distinct powers of 3, otherwise return **false**.
 
-📌 Example:
+ Example:
 Input: n = 12
 Output: true
 Explanation: 12 = 3^0 + 3^1 + 3^2
 
-🔍 Approach:
+ Approach:
 - First step is to Convert `n` into **base-3 representation**.
 - If any digit in base-3 is **2**, then return `false` (because we can not use the same power twice).
 - Otherwise, return `true`.
